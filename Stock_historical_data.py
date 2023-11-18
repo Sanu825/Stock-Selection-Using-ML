46a1f4256d02c78e1d2cee75dfea16c9f4061e72
Sanu Das <SANUD445@HOTMAIL.COM>
2023-11-18 21:29:56 +0530

    Add Banking Sector historical data

--- Stock_historical_data.py
+++ Stock_historical_data.py
@@ -4,10 +4,15 @@ import datetime
 from pathlib import Path    # Import the pathlib library
 
 # Replace 'Stock_symbol' with the stock symbol you are interested in
-symbol = 'UPL.NS'
+# Private Bank stock symbol
+# symbol = 'SURYODAY.NS'
+# symbol = ['AUBANK.NS', 'AXISBANK.NS', 'BANDHANBNK.NS', 'CSBBANK.NS', 'CUB.NS', 'DCBBANK.NS', 'DHANBANK.NS', 'EQUITASBNK.NS', 'ESAFSFB.NS', 'FEDERALBNK.NS', 'FINOPB.NS', 'HDFCBANK.NS', 'ICICIBANK.NS', 'IDBI.NS', 'IDFCFIRSTB.NS', 'INDUSINDBK.NS', 'J&KBANK.NS', 'KARURVYSYA.NS', 'KOTAKBANK.NS', 'KTKBANK.NS', 'RBLBANK.NS', 'SOUTHBANK.NS', 'SURYODAY.NS', 'TMB.NS', 'UJJIVANSFB.NS', 'UTKARSHBNK.NS', 'YESBANK.NS' ]
+
+# Public Bank stock symbol
+symbol = ['BANKBARODA.NS', 'BANKINDIA.NS', 'CANBK.NS', 'CENTRALBK.NS', 'INDIANB.NS', 'IOB.NS', 'MAHABANK.NS', 'PNB.NS', 'PSB.NS', 'SBIN.NS', 'UCOBANK.NS, UNIONBANK.NS']
 
 # Specify the directory path where you want to save the file
-download_directory = Path("D:\Programming\Stock Selections\Stock-Selection-Using-ML\Collect_Stock_Historical_Data\Banking_Sector")  # Replace with your desired directory
+download_directory = Path("D:\Programming\Stock Selections\Stock-Selection-Using-ML\Collect_Stock_Historical_Data\Banking_Sector\Public_Bank")  # Replace with your desired directory
 
 # Calculate the end date as today
 end_date = pd.to_datetime('today').strftime('%Y-%m-%d')
@@ -16,10 +21,11 @@ end_date = pd.to_datetime('today').strftime('%Y-%m-%d')
 start_date = (pd.to_datetime('today') - pd.DateOffset(years=4)).replace(month=1, day=1).strftime('%Y-%m-%d')
 
 # Download historical data from Yahoo Finance for the last 5 years
-stock_data = yf.download(symbol, start=start_date, end=end_date)
+for symbol in symbol:
+    stock_data = yf.download(symbol, start=start_date, end=end_date)
 
-# Save the data to a CSV file in the specified directory
-csv_file_path = download_directory / f'{symbol}_historical_data.csv'
-stock_data.to_csv(csv_file_path)
+    # Save the data to a CSV file in the specified directory
+    csv_file_path = download_directory / f'{symbol}_historical_data.csv'
+    stock_data.to_csv(csv_file_path)
 
 
