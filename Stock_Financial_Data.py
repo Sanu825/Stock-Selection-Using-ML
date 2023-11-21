ab7dcf58fde5b1c8e1cc9897fffe057a2df9ec77
Sanu Das <SANUD445@HOTMAIL.COM>
2023-11-21 17:32:19 +0530

    Added Us_stock financial data

--- Stock_Financial_Data.py
+++ Stock_Financial_Data.py
@@ -24,7 +24,7 @@ from datetime import datetime, timedelta
 #            'NESTLEIND.NS', 'NTPC.NS', 'ONGC.NS', 'POWERGRID.NS', 'RELIANCE.NS', 'SBILIFE.NS', 'SBIN.NS', 'SUNPHARMA.NS', 'TATACONSUM.NS', 'TATAMOTORS.NS', 'TATASTEEL.NS', 'TCS.NS', 'TECHM.NS', 
 #            'TITAN.NS', 'ULTRACEMCO.NS', 'UPL.NS', 'WIPRO.NS']
 
-tickers = ['MSFT']
+tickers = ['MSFT', 'AAPL', 'GOOGL', 'AMZN']
 # Specify the base folder where stock folders will be created
 file_path = Path('D:/Programming/Stock Selections/Stock-Selection-Using-ML/Collect_Stock_Financial_Data/US_Stock')
 
