b1facf0d3b77ee9fea72293b10a644ab5ceabcb6
Sanu Das <SANUD445@HOTMAIL.COM>
2023-11-21 15:46:02 +0530

    Add financial data

--- Stock_Finacial_Data_Through_API.py
+++ Stock_Finacial_Data_Through_API.py
@@ -4,74 +4,71 @@ from pathlib import Path
 from datetime import datetime, timedelta
 
 # Replace 'YOUR_API_KEY' with your actual Alpha Vantage API key
-api_key = '9XW0SN1H49A7N4KH'
+api_key = '7BWY5GGU94GAB627'
 
 # Replace 'AAPL' with the stock symbol you are interested in
-symbol = 'IBN'
+symbols = ['HDB', 'SBI']
 
 # Specify the parent directory path where you want to save the files
 parent_directory = Path("D:/Programming/Stock Selections/Stock-Selection-Using-ML/Collect_Stock_Financial_Data")
 
+
+
 # Create a directory for the specific stock if it doesn't exist
-stock_directory = parent_directory / symbol
-stock_directory.mkdir(parents=True, exist_ok=True)
+for symbol in symbols:
+    stock_directory = parent_directory / symbol
+    stock_directory.mkdir(parents=True, exist_ok=True)
 
-# Function to make API request and save data to a JSON file
-def save_alpha_vantage_data(url, file_path):
-    response = requests.get(url)
-    data = response.json()
-    with open(file_path, 'w') as file:
-        json.dump(data, file)
-    print(f"Data saved to {file_path}")
+    # Function to make API request and save data to a JSON file
+    def save_alpha_vantage_data(url, file_path):
+        response = requests.get(url)
+        data = response.json()
+        with open(file_path, 'w') as file:
+            json.dump(data, file)
+        print(f"Data saved to {file_path}")
 
-# Calculate the end date as today
-end_date = datetime.now().strftime('%Y-%m-%d')
 
-# Calculate the start date as 6 years ago, rounded to the beginning of the quarter
-start_date = (datetime.now() - timedelta(days=7*365.25)).replace(
-    month=datetime.now().month - (datetime.now().month - 1) % 3, day=1
-).strftime('%Y-%m-%d')
+    # Calculate the end date as today
+    end_date = datetime.now().strftime('%Y-%m-%d')
 
-# Create a dictionary to store all financial data and metrics
-financial_data = {}
+    # Calculate the start date as 6 years ago, rounded to the beginning of the quarter
+    start_date = (datetime.now() - timedelta(days=5*365.25)).replace(month=datetime.now().month - (datetime.now().month - 1) % 3, day=1).strftime('%Y-%m-%d')
 
-# Download balance sheet, income statement, and cash flow data
-balance_sheet_url = f'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol={symbol}&apikey={api_key}&interval=quarterly'
-income_statement_url = f'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={symbol}&apikey={api_key}&interval=quarterly'
-cash_flow_url = f'https://www.alphavantage.co/query?function=CASH_FLOW&symbol={symbol}&apikey={api_key}&interval=quarterly'
+    # Create a dictionary to store all financial data and metrics
+    financial_data = {}
 
-# # Create a directory for the specific stock if it doesn't exist
-# stock_directory = parent_directory / symbol
-# stock_directory.mkdir(parents=True, exist_ok=True)
+    # Download balance sheet, income statement, and cash flow data
+    balance_sheet_url = f'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol={symbol}&apikey={api_key}&interval=quarterly'
+    income_statement_url = f'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={symbol}&apikey={api_key}&interval=quarterly'
+    cash_flow_url = f'https://www.alphavantage.co/query?function=CASH_FLOW&symbol={symbol}&apikey={api_key}&interval=quarterly'
+    overview_url = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={symbol}&apikey={api_key}'
 
-# Download balance sheet data
-balance_sheet_file_path = stock_directory / f'{symbol}_balance_sheet.json'
-save_alpha_vantage_data(balance_sheet_url, balance_sheet_file_path)
-with open(balance_sheet_file_path, 'r') as file:
-    financial_data['balance_sheet'] = json.load(file)
 
-# Download income statement data
-income_statement_file_path = stock_directory / f'{symbol}_income_statement.json'
-save_alpha_vantage_data(income_statement_url, income_statement_file_path)
-with open(income_statement_file_path, 'r') as file:
-    financial_data['income_statement'] = json.load(file)
+    # # Create a directory for the specific stock if it doesn't exist
+    # stock_directory = parent_directory / symbol
+    # stock_directory.mkdir(parents=True, exist_ok=True)
 
-# Download cash flow data
-cash_flow_file_path = stock_directory / f'{symbol}_cash_flow.json'
-save_alpha_vantage_data(cash_flow_url, cash_flow_file_path)
-with open(cash_flow_file_path, 'r') as file:
-    financial_data['cash_flow'] = json.load(file)
+    # Download balance sheet data
+    balance_sheet_file_path = stock_directory / f'{symbol}_balance_sheet.json'
+    save_alpha_vantage_data(balance_sheet_url, balance_sheet_file_path)
+    with open(balance_sheet_file_path, 'r') as file:
+        financial_data['balance_sheet'] = json.load(file)
 
-# Download additional financial metrics
-overview_url = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={symbol}&apikey={api_key}'
-overview_file_path = stock_directory / f'{symbol}_overview.json'
-save_alpha_vantage_data(overview_url, overview_file_path)
-with open(overview_file_path, 'r') as file:
-    financial_data['overview'] = json.load(file)
+    # Download income statement data
+    income_statement_file_path = stock_directory / f'{symbol}_income_statement.json'
+    save_alpha_vantage_data(income_statement_url, income_statement_file_path)
+    with open(income_statement_file_path, 'r') as file:
+        financial_data['income_statement'] = json.load(file)
 
-# Save all financial data and metrics to a single JSON file
-all_data_file_path = stock_directory / f'{symbol}_all_financial_data.json'
-with open(all_data_file_path, 'w') as file:
-    json.dump(financial_data, file)
+    # Download cash flow data
+    cash_flow_file_path = stock_directory / f'{symbol}_cash_flow.json'
+    save_alpha_vantage_data(cash_flow_url, cash_flow_file_path)
+    with open(cash_flow_file_path, 'r') as file:
+        financial_data['cash_flow'] = json.load(file)
 
-print(f"All financial data for {symbol} saved to {all_data_file_path}")
+    # Download additional financial metrics
+    overview_file_path = stock_directory / f'{symbol}_overview.json'
+    save_alpha_vantage_data(overview_url, overview_file_path)
+    with open(overview_file_path, 'r') as file:
+        financial_data['overview'] = json.load(file)
+
---
+++ Stock_Financial_Data.py
@@ -0,0 +1,49 @@
+import yfinance as yf
+import pandas as pd
+from pathlib import Path
+from datetime import datetime, timedelta
+
+# Replace 'ticker' with the stock symbol you are interested in
+# tickers = ['AUBANK.NS', 'AXISBANK.NS', 'BANDHANBNK.NS', 'CSBBANK.NS', 'CUB.NS', 'DCBBANK.NS', 'DHANBANK.NS', 'EQUITASBNK.NS', 'ESAFSFB.NS', 'FEDERALBNK.NS', 'FINOPB.NS', 'HDFCBANK.NS', 'ICICIBANK.NS', 'IDBI.NS', 'IDFCFIRSTB.NS', 'INDUSINDBK.NS', 'J&KBANK.NS', 'KARURVYSYA.NS', 'KOTAKBANK.NS', 'KTKBANK.NS', 'RBLBANK.NS', 'SOUTHBANK.NS', 'SURYODAY.NS', 'TMB.NS', 'UJJIVANSFB.NS', 'UTKARSHBNK.NS', 'YESBANK.NS' ]
+# tickers = ['BANKBARODA.NS', 'BANKINDIA.NS', 'CANBK.NS', 'CENTRALBK.NS', 'INDIANB.NS', 'IOB.NS', 'MAHABANK.NS', 'PNB.NS', 'PSB.NS', 'SBIN.NS', 'UCOBANK.NS', 'UNIONBANK.NS']
+tickers = ['AFFLE.NS', 'BSOFT.NS', 'CAMS.NS', 'CMSINFO.NS', 'COFORGE.NS', 'CYIENT.NS', 'ECLERX.NS', 'FSL.NS', 'HAPPSTMNDS.NS', 'HCLTECH.NS', 'INFY.NS', 'INTELLECT.NS', 'KPITTECH.NS', 'LTIM.NS', 'LTTS.NS', 'MASTEK.NS', 'MPHASIS.NS', 'NAUKRI.NS', 'NEWGEN.NS', 'OFSS.NS', 'PERSISTENT.NS', 'RATEGAIN.NS', 'ROUTE.NS', 'SONATSOFTW.NS', 'TANLA.NS', 'TATAELXSI.NS', 'TCS.NS', 'TECHM.NS', 'TEJASNET.NS', 'WIPRO.NS', 'ZENSARTECH.NS', 'ZENTEC.NS']
+
+# Specify the base folder where stock folders will be created
+file_path = Path('D:/Programming/Stock Selections/Stock-Selection-Using-ML/Collect_Stock_Financial_Data')
+
+# Iterate over each stock
+for ticker in tickers:
+    # Create a folder for the stock if it doesn't exist
+    stock_folder = file_path / ticker
+    stock_folder.mkdir(parents=True, exist_ok=True)
+
+    # Print the stock symbol before saving each file
+    print(f"Downloading data for stock: {ticker}")
+
+    # Get the balance sheet for the stock
+    balance_sheet = yf.Ticker(ticker).balance_sheet
+
+    # Save balance sheet data in a CSV file
+    balance_sheet_csv_path = stock_folder / f'{ticker}_balance_sheet.csv'
+    balance_sheet.to_csv(balance_sheet_csv_path)
+    
+
+    # Get the cash flow statement for the stock
+    cash_flow = yf.Ticker(ticker).cashflow
+
+    # Save cash flow data in a CSV file
+    cash_flow_csv_path = stock_folder / f'{ticker}_cash_flow.csv'
+    cash_flow.to_csv(cash_flow_csv_path)
+    
+
+    # Get the profit & loss statement for the stock
+    profit_loss = yf.Ticker(ticker).financials
+
+    # Save profit & loss data in a CSV file
+    profit_loss_csv_path = stock_folder / f'{ticker}_profit_loss.csv'
+    profit_loss.to_csv(profit_loss_csv_path)
+    
+
+    # Specify the time range for the last 4 years
+    end_date = datetime.now().strftime('%Y-%m-%d')
+    start_date = (datetime.now() - timedelta(days=5*365)).strftime('%Y-%m-%d')
